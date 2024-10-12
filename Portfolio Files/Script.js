f3b2f52c85e9cab57956404ec0079167515d2aa0
Yenuganti Vijay Kumar <yenugantivijaykumar@gmail.com>
2024-10-12 17:43:13 +0530

    Update Script.js

--- Portfolio Files/Script.js
+++ Portfolio Files/Script.js
@@ -1,58 +1,65 @@
-/*==========\ For Toggle Icon /==========*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+/**
+ * ==========\ For Toggle Icon /==========
+ *
+ * @format
+ */
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('fa-xmark');
-  navbar.classList.toggle('active')
-}
-
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
 
 /*==========\ Scroll Section active link /==========*/
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navlinks.forEach(link => {
-        link.classList.remove('active');
-        document.querySelector(`header nav a[href="#${id}"]`).classList.add('active');
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(`header nav a[href="#${id}"]`)
+          .classList.add("active");
       });
-    };
+    }
   });
 
   /*==========\ Sticky NavBar /==========*/
-  let header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 100);
-
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
   /*==========\ Sticky NavBar /==========*/
-  menuIcon.classList.remove('fa-xmark');
-  navbar.classList.remove('active');
+  menuIcon.classList.remove("fa-xmark");
+  navbar.classList.remove("active");
 };
 
 /*==========\ Scroll Reveal /==========*/
 ScrollReveal({
-  distance: '80px',
+  distance: "80px",
   duration: 2000,
   delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-contanier, .portfolio-box, .content form', { origin: 'buttom' });
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
-
+ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .content form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-contact h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-contact p, .about-content", { origin: "right" });
 
 /*==========\ Typed JS /==========*/
-const typed = new Typed('.multiple-text', {
-  strings: ['Full Stack Developer', 'Web Designer'],
+const typed = new Typed(".multiple-text", {
+  strings: ["Full Stack Developer", "Web Designer", "UI/UX Designer"],
   typeSpeed: 70,
   backSpeed: 70,
   backDelay: 1000,
